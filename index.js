2de2048b24c8004490916811ae1152add1644f3d
ramon morcillo <ramonmorcillocascales@gmail.com>
2019-09-17 06:35:53 +0200

    getting the script tag

--- index.js
+++ index.js
@@ -3,4 +3,15 @@ const cheerio = require('cheerio');
 
 (async() => {
 
+    const USERNAME = 'willsmith';
+    const BASE_URL = `https://instagram.com/${USERNAME}`;
+
+    let response = await request(BASE_URL);
+
+    let $ = cheerio.load(response);
+
+    // eq(3) because I want the fourth script tag
+    let script = $('script[type="text/javascript"]').eq(3).html();
+
+    console.log(script);
 })()

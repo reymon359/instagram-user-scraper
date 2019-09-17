a31917dd85ee0a49d92d8e1d7be324d13922ed7b
ramon morcillo <ramonmorcillocascales@gmail.com>
2019-09-17 06:50:01 +0200

    getting the user instagram data

--- index.js
+++ index.js
@@ -13,5 +13,24 @@ const cheerio = require('cheerio');
     // eq(3) because I want the fourth script tag
     let script = $('script[type="text/javascript"]').eq(3).html();
 
-    console.log(script);
+    let script_regex = /window._sharedData = (.+);/g.exec(script);
+
+    // object.entry_data.ProfilePage[0].graphql.user 
+    let {
+        entry_data: {
+            ProfilePage: {
+                [0]: { graphql: { user } }
+            }
+        }
+    } = JSON.parse(script_regex[1]);
+
+    let instagram_data = {
+        followers: user.edge_followed_by.count,
+        following: user.edge_follow.count,
+        uploads: user.edge_owner_to_timeline_media.count,
+        full_name: user.full_name,
+        picture_url: user.profile_pic_url_hd
+    }
+
+    console.log(instagram_data);
 })()

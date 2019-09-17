199d59f03820709434b00a7d32fa004e4cf4f6f5
ramon morcillo <ramonmorcillocascales@gmail.com>
2019-09-17 07:06:15 +0200

    Instagram data scraper done

--- index.js
+++ index.js
@@ -15,21 +15,36 @@ const cheerio = require('cheerio');
 
     let script_regex = /window._sharedData = (.+);/g.exec(script);
 
-    // object.entry_data.ProfilePage[0].graphql.user 
-    let {
-        entry_data: {
-            ProfilePage: {
-                [0]: { graphql: { user } }
-            }
-        }
-    } = JSON.parse(script_regex[1]);
+    let { entry_data: { ProfilePage: { [0]: { graphql: { user } } } } } = JSON.parse(script_regex[1]);
+
+    // Scraping posts
+    let { entry_data: { ProfilePage: { [0]: { graphql: { user: { edge_owner_to_timeline_media: { edges } } } } } } } = JSON.parse(script_regex[1]);
+
+    let posts = [];
+
+    for (let edge of edges) {
+        let { node } = edge;
+
+        posts.push({
+            id: node.id,
+            shortcode: node.shortcode,
+            timestamp: node.taken_at_timestamp,
+            likes: node.edge_liked_by.count,
+            comments: node.edge_media_to_comment.count,
+            video_views: node.video_view_count,
+            caption: node.edge_media_to_caption.edges[0].node.text,
+            image_url: node.display_url
+        });
+
+    }
 
     let instagram_data = {
         followers: user.edge_followed_by.count,
         following: user.edge_follow.count,
         uploads: user.edge_owner_to_timeline_media.count,
         full_name: user.full_name,
-        picture_url: user.profile_pic_url_hd
+        picture_url: user.profile_pic_url_hd,
+        posts
     }
 
     console.log(instagram_data);
